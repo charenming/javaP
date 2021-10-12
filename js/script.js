01384bfcd7a9378b4ff29ec7857175d352c8d8c4
charenming <ysem88@gmail.com>
2021-10-13 00:16:37 +0900

    javaP

--- js/script.js
+++ js/script.js
@@ -1,14 +1,24 @@
+// var swiper = new swiper(".mySwiper", {
+//   slidesPerView: 1,
+//   spaceBetween: 30,
+//   loop: true,
+//   effect: "fade",
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+// });
 
-      var swiper = new Swiper(".mySwiper", {
-        slidesPerView: 1,
-        spaceBetween: 30,
-        loop: true,
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
-        },
-        navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        },
-      });
+// 스크롤 높이에 따른
+$(window).scroll(function (evt) {
+  var y = $(this).scrollTop();
+  if (y > 120) {
+    $(".header").removeClass(".gnbBg");
+  } else {
+    $(".header").addClass(".gnbBg");
+  }
+});
