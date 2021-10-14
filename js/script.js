02f43feadb10c588674f39f84f7229caafeab390
charenming <ysem88@gmail.com>
2021-10-14 10:53:07 +0900

    javaP

--- js/script.js
+++ js/script.js
@@ -1,24 +1,28 @@
-// var swiper = new swiper(".mySwiper", {
-//   slidesPerView: 1,
-//   spaceBetween: 30,
-//   loop: true,
-//   effect: "fade",
-//   pagination: {
-//     el: ".swiper-pagination",
-//     clickable: true,
-//   },
-//   navigation: {
-//     nextEl: ".swiper-button-next",
-//     prevEl: ".swiper-button-prev",
-//   },
-// });
+// 메뉴 스크롤
+$(function () {
+  $(window).scroll(function (evt) {
+    var y = $(this).scrollTop();
+    if (y > 120) {
+      $(".header").addClass("gnbBg");
+      $(".gnb>li>a,.liveReservation>a>i").addClass("gnbColor");
+    } else {
+      $(".header").removeClass("gnbBg");
+      $(".gnb>li>a,.liveReservation>a>i").removeClass("gnbColor");
+    }
+  });
 
-// 스크롤 높이에 따른
-$(window).scroll(function (evt) {
-  var y = $(this).scrollTop();
-  if (y > 120) {
-    $(".header").removeClass("gnbBg");
-  } else {
-    $(".header").addClass("gnbBg");
-  }
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    effect: "fade",
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
 });
